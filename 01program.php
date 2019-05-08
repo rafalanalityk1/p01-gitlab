9d0b5407038a264dbd86678864a167cd813afadd
rafalanalityk1 <50425319+rafalanalityk1@users.noreply.github.com>
2019-05-08 22:36:28 +0200

    Piąty commit

--- 01program.php
+++ 01program.php
@@ -1,4 +1,8 @@
 <?php
 echo ("Witaj");
+echo ("<br> Jestem programem 01");
+echo ("<br> Zrobione ");
+echo ("<br> Kolejna Linijka dopisana");
+echo("<br>Dalsza czesc programu");
+echo("<br>Dalsza czesc programu z  githuba;d");
 ?>
-
